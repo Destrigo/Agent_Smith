2840e14d9cbae219effbdef7e8e6a94d1fe4cdd4
Marco Tarantino <marco.tarantino.bg@gmail.com>
2026-05-14 17:04:06 +0200

    basic basic sandbox

---
+++ sandbox/core/sandbox.py
@@ -0,0 +1,34 @@
+import io
+from contextlib import redirect_stdout
+
+
+class Sandbox:
+    def __init__(self):
+        self.tools = {
+            "hello": 0
+        }
+
+    def execute(self, code: str):
+        stdout_buffer = io.StringIO()
+
+        sandbox_globals = {
+            "__builtins__": __builtins__,
+            **self.tools
+        }
+
+        try:
+            with redirect_stdout(stdout_buffer):
+                exec(code, sandbox_globals)
+
+            return {
+                "success": True,
+                "stdout": stdout_buffer.getvalue(),
+                "error": None
+            }
+
+        except Exception as e:
+            return {
+                "success": False,
+                "stdout": stdout_buffer.getvalue(),
+                "error": str(e)
+            }
